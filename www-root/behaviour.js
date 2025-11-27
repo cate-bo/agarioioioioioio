d45467476d8e67403ee33c6d924fe0b641a4f52f
Matthias Utrata <matthias.utrata@outlook.com>
2025-11-27 08:04:59 +0100

    Zoom added

--- www-root/behaviour.js
+++ www-root/behaviour.js
@@ -10,6 +10,8 @@ let circle;
 let context;
 let offsetY;
 let offsetX;
+let scale = 1;
+let screen;
 document.addEventListener("DOMContentLoaded", function() {
     playingField = document.getElementById("playArea");
     playingField.width = 10000;
@@ -18,6 +20,7 @@ document.addEventListener("DOMContentLoaded", function() {
     context = playingField.getContext("2d");
     offsetY = (playingField.height/2);
     offsetX = (playingField.width/2);
+    screen = document.getElementById("whole");
 
     inputField = document.getElementById("nameInput");
     inputField.setAttribute("placeholder", "Add name here...");
@@ -27,13 +30,31 @@ document.addEventListener("DOMContentLoaded", function() {
         startGame();
     });
     drawBoard();
+
 });
 
 function startGame(){
     menuPlatform = document.getElementById("screen");
     menuPlatform.style.display = "none";
+    window.scrollTo(800, 1000);
+    document.addEventListener("wheel", (event) => {
+        event.preventDefault();
+        scale += event.deltaY * -0.0009;
+        scale = Math.min(Math.max(0.85, scale), 1.6);
+        screen.style.transform = `scale(${scale})`;
+        console.log("scale", scale);
+    }, {passive: false});
+    play();
 }
 
+function play() {
+    context.beginPath();
+    context.arc(800, 1000,40,0,2*Math.PI);
+    context.fillStyle = "red";
+    context.fill();
+}
+
+//Funktion für das Zeichnen des Grids auf dem Spielfeld
 function drawBoard(){
     for (let x = 0; x <= playingField.width+offsetX; x += 40) {
         context.moveTo(0.5 + x + 10, 10);
